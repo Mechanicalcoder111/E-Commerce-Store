ae5b8b2c5b325012f969bec831ae25556844e324
Mechanicalcoder111 <160551936+Mechanicalcoder111@users.noreply.github.com>
2025-12-11 16:20:32 -0600

    Delete CSCI467-Project-master/frontend directory

--- CSCI467-Project-master/frontend/app/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer style={{ textAlign: 'center', padding: '1rem', borderTop: '1px solid #ccc' }}>
-      <p>Created by Team Members: Shaivil, Alex, Antonio, James, Aasim</p>
-    </footer>
-  );
-};
-
-export default Footer;
